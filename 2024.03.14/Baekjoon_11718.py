3f55670503f48bda0ff4103761952cdb08878921
carelesspark <kiae1234@naver.com>
2024-03-14 16:29:38 +0900

    # 2024.03.14 Study ) 백준 11718번 문제

---
+++ 2024.03.14/Baekjoon_11718.py
@@ -0,0 +1,12 @@
+# 백준 11718번 문제
+import sys
+
+try:
+    while True:
+        word = sys.stdin.readline().rstrip()
+        print(word)
+        if word == "":
+            break
+except:
+    exit()
+
