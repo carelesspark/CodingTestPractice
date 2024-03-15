53b77df18d74ad748d81267b4404cd39b32be2ff
carelesspark <kiae1234@naver.com>
2024-03-15 17:56:10 +0900

    # 2024.03.15 Study ) 백준 14645번 문제

---
+++ 2024.03.15/Baekjoon_14645.py
@@ -0,0 +1,9 @@
+# 백준 14645번 문제
+import sys
+
+n, k = map(int, sys.stdin.readline().rstrip().split())
+
+for i in range(n):
+    a, b = map(int, sys.stdin.readline().rstrip().split())
+
+print("비와이")
