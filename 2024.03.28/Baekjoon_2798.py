db33f07d8a5e04c63d465ea94462c94636f864e9
carelesspark <kiae1234@naver.com>
2024-03-28 16:54:56 +0900

    # 2024.03.28 Study ) 백준 2798번 문제

--- 2024.03.28/Baekjoon_2798.py
+++ 2024.03.28/Baekjoon_2798.py
@@ -1,16 +1,16 @@
 # 백준 2798번 문제
-import sys, itertools
+# import sys, itertools
 
-n, m = map(int, sys.stdin.readline().rstrip().split())
+# n, m = map(int, sys.stdin.readline().rstrip().split())
 
-card = list(map(int, sys.stdin.readline().rstrip().split()))
+# card = list(map(int, sys.stdin.readline().rstrip().split()))
 
-result = 0
+# result = 0
 
-for comb in itertools.combinations(card, 3):
-    if sum(comb) <= m:
-        result = max(result, sum(comb))
-print(result)
+# for comb in itertools.combinations(card, 3):
+#     if sum(comb) <= m:
+#         result = max(result, sum(comb))
+# print(result)
 
 # ----------------------------------------------------------
 # for문으로 푸는 방법도 있지만, itertools 라이브러리의 combinations 함수를 이용해서 문제를 풀 수 있다.
@@ -18,3 +18,20 @@ print(result)
 # 그렇게 나온 조합을 sum하고 민액 m의 이하 값일 경우 result에 넣는데, 이때 배열에 값을 저장해도 되지만, max(a, b)를 통해 기존 result 값이 크면 result를, 방금 만든 sum(comb)값이 크면
 # result에 sum(comb)값을 저장하는 형식으로 만든다.
 # 그렇게 for문이 끝나면 m을 넘지 않는 합계 중 가장 큰 값을 출력할 수 있다.
+
+import sys
+
+n, m = map(int, sys.stdin.readline().rstrip().split())
+
+card = list(map(int, sys.stdin.readline().rstrip().split()))
+result = 0 
+
+for i in range(n):
+    for j in range(i + 1, n):
+        for k in range(j + 1, n):
+            if card[i] + card[j] + card[k] <= m:
+                result = max(result, card[i] + card[j] + card[k])
+
+print(result)
+
+# 위의 방식은 for문을 이용한 방식이다.
