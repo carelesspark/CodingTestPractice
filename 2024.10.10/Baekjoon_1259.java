2ee8884e3d11c53fc17af20ba8a21c7c0c35ab24
CareLess <kiae1234@naver.com>
2024-10-10 17:11:54 +0900

    # 2024.10.10 Study ) 백준 1259번 문제

---
+++ 2024.10.10/Baekjoon_1259.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Baekjoon_1259 {
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+
+        while (true) {
+            StringBuilder sb = new StringBuilder();
+
+            String n = br.readLine();
+            
+            if(n.equals("0")){
+                break;
+            }
+
+            String reversedN = sb.append(n).reverse().toString();
+
+            if(n.equals(reversedN)) {
+                System.out.println("yes");
+            } else {
+                System.out.println("no");
+            }
+        }
+    }
+}
