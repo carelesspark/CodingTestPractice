5c6d49850e2ab6f1e0a1d4231811fbb4442a20d5
CareLess <kiae1234@naver.com>
2024-10-22 01:07:53 +0900

    # 2024.10.21 Study ) 백준 4796번 문제

---
+++ 2024.10.21/Baekjoon_4796.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.io.*;
+
+public class Baekjoon_4796 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = 1;
+
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int L = Integer.parseInt(st.nextToken());
+            int P = Integer.parseInt(st.nextToken());
+            int V = Integer.parseInt(st.nextToken());
+
+            if(L == 0 && P == 0 && V == 0) {
+                return;
+            }
+
+
+            if(V % P == 0) {
+                System.out.println("Case " + T +": " +  (V / P) * L);
+            } else {
+                if((V % P) > L){
+                    System.out.println("Case " + T +": " +  (((V / P) * L) + L));
+                } else {
+                    System.out.println("Case " + T +": " +  (((V / P) * L) + (V % P)));
+                }
+                
+            }
+
+            T++;
+        }
+    }
+}
