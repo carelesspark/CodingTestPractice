4337f6ff1432fc9edeefbc6a0833f678a2a90589
CareLess <kiae1234@naver.com>
2024-12-02 11:40:26 +0900

    # 2024.12.02 Study ) 백준 1476번 문제

---
+++ 2024.12.02/Baekjoon_1476.java
@@ -0,0 +1,47 @@
+import java.util.*;
+import java.io.*;
+
+public class Baekjoon_1476 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int E = Integer.parseInt(st.nextToken());
+        int S = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int tempE = 0;
+        int tempS = 0;
+        int tempM = 0;
+        int year = 0;
+
+
+        while (true) {
+            year++;
+
+            if(tempE >= 15) {
+                tempE = 0;
+            }
+
+            if(tempS >= 28) {
+                tempS = 0;
+            }
+
+            if(tempM >= 19) {
+                tempM = 0;
+            }
+
+            tempE++;
+            tempS++;
+            tempM++;
+
+            System.out.println(tempE + ", " + tempS + ", " + tempM);
+
+            if(E == tempE && S == tempS && M == tempM) {
+                break;
+            }
+        }
+
+        System.out.println(year);
+    }
+}
