b86643772aee9d647e2044ea995908185fc3a61a
CareLess <kiae1234@naver.com>
2024-12-05 17:52:09 +0900

    # 2024.12.05 Study ) 백준 2740번 문제

--- 2024.12.05/Baekjoon_2740.java
+++ 2024.12.05/Baekjoon_2740.java
@@ -5,7 +5,6 @@ public class Baekjoon_2740 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
         int[][] tempA = new int[N][M];
