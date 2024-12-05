e7c99bc9d954e7f607103a2667e44bc2ac86d09a
CareLess <kiae1234@naver.com>
2024-12-05 17:51:07 +0900

    # 2024.12.03 Study ) 백준 10867번 문제

---
+++ 2024.12.05/Baekjoon_10867.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+
+public class Baekjoon_10867 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] temp = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for(int i = 0; i < N; i++) {
+            temp[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        int[] arr = Arrays.stream(temp).distinct().sorted().toArray();
+
+        StringBuilder sb = new StringBuilder();
+        for(int n : arr) {
+            sb.append(n).append(" ");
+        }
+
+        System.out.println(sb.toString().trim());
+    }
+}
