564aad49929764b85fbe9432de1fe6bd9403a5fa
CareLess <kiae1234@naver.com>
2024-12-09 02:25:57 +0900

    # 2024.12.09 Study ) 백준 24313번 문제

---
+++ 2024.12.09/Baekjoon_24313.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+public class Baekjoon_24313 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int a1 = Integer.parseInt(st.nextToken());
+        int a0 = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+
+        if(a1 * n + a0 <= c * n && c >= a1) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+    }
+}
