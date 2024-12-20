59cea618e120a7d21df9dd43d6dce9eae2dd9249
CareLess <kiae1234@naver.com>
2024-12-20 17:52:47 +0900

    # 2024.12.20 Study ) 백준 2606번 문제

--- 2024.12.20/Baekjoon_2606.java
+++ 2024.12.20/Baekjoon_2606.java
@@ -2,17 +2,21 @@ import java.util.*;
 import java.io.*;
 
 public class Baekjoon_2606 {
+    static List<List<Integer>> graph;
+    static boolean[] visited;
+    static int count = 0;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int M = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
 
-        List<List<Integer>> graph = new ArrayList<>();
-        for(int i = 0; i <= N; i++) {
+        graph = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
             graph.add(new ArrayList<>());
         }
 
-        for(int i = 0; i < M; i++) {
+        for (int i = 0; i < m; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
@@ -20,24 +24,63 @@ public class Baekjoon_2606 {
             graph.get(b).add(a);
         }
 
-        boolean[] visited = new boolean[N + 1];
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(1);
-        visited[1] = true;
+        visited = new boolean[n + 1];
+        dfs(1);
+        System.out.println(count - 1); // 1번 컴퓨터 제외
+    }
 
-        int count = 0;
+    public static void dfs(int node) {
+        visited[node] = true;
+        count++;
 
-        while (!queue.isEmpty()) {
-            int current = queue.poll();
-            for(int neighbor : graph.get(current)) {
-                if(!visited[neighbor]) {
-                    visited[neighbor] = true;
-                    queue.add(neighbor);
-                    count++;
-                }
+        for (int neighbor : graph.get(node)) {
+            if (!visited[neighbor]) {
+                dfs(neighbor);
             }
         }
-
-        System.out.println(count);
     }
 }
+
+// import java.util.*;
+// import java.io.*;
+
+// public class Baekjoon_2606 {
+//     public static void main(String[] args) throws IOException {
+//         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//         int N = Integer.parseInt(br.readLine());
+//         int M = Integer.parseInt(br.readLine());
+
+//         List<List<Integer>> graph = new ArrayList<>();
+//         for(int i = 0; i <= N; i++) {
+//             graph.add(new ArrayList<>());
+//         }
+
+//         for(int i = 0; i < M; i++) {
+//             StringTokenizer st = new StringTokenizer(br.readLine());
+//             int a = Integer.parseInt(st.nextToken());
+//             int b = Integer.parseInt(st.nextToken());
+//             graph.get(a).add(b);
+//             graph.get(b).add(a);
+//         }
+
+//         boolean[] visited = new boolean[N + 1];
+//         Queue<Integer> queue = new LinkedList<>();
+//         queue.add(1);
+//         visited[1] = true;
+
+//         int count = 0;
+
+//         while (!queue.isEmpty()) {
+//             int current = queue.poll();
+//             for(int neighbor : graph.get(current)) {
+//                 if(!visited[neighbor]) {
+//                     visited[neighbor] = true;
+//                     queue.add(neighbor);
+//                     count++;
+//                 }
+//             }
+//         }
+
+//         System.out.println(count);
+//     }
+// }
